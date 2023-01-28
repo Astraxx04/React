e72c54392a850deedc63282cc61d2e249b2b0ce2
Astraxx04 <gagan200254@gmail.com>
2023-01-28 15:14:33 +0530

    Started react

---
+++ firstapp/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function Heading() {
+  let title="This is some heading text";
+  return (
+    <hi>{ title }</hi>
+  )
+}
+
+
+function App() {
+  return <Heading />;
+}
+
+export default App;
